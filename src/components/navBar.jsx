f47dd315db899b272df79f713c7bf2b75d877306
kushana <rashmisenadheera98@gmail.com>
2024-11-19 23:30:01 +0530

    Basic NavBAr Structure

---
+++ src/components/navBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className='nav'>
+        <nav>
+            <ul>
+                <li><img></img></li>
+                <li>Dashboard</li>
+                <li>Books</li>
+                <li>Members</li>
+                <li>About Us</li>
+                <li><button>Logout</button></li>
+
+            </ul>
+        </nav>
+      
+    </div>
+  );
+};
+
+export default NavBar;
