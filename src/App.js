b8b613288f40fb928f0d24176b8fe9560f82e8a8
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-11-28 08:47:33 +0530

    import issueBook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/navBar';
+import IssueBook from './components/issueBook/issueBook';
 
 function App() {
   return (
     <div className="App">
      <NavBar />
+     <IssueBook />
     </div>
   );
 }
