3248db48b3db585be7864102d806f73c10ea1b45
kushana <rashmisenadheera98@gmail.com>
2024-12-02 11:55:51 +0530

    Created basic home structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import NavBar from './components/navBar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-     <NavBar/>
+     <Home/>
     </div>
   );
 }
