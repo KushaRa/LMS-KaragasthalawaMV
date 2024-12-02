1fb0f13996235ca87522c2660d0dd0581387c46e
kushana <rashmisenadheera98@gmail.com>
2024-12-02 12:08:37 +0530

    Merge branch 'master' of https://github.com/KushaRa/LMS-Karagasthalawa-M.V. into feat1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './pages/Home';
+import IssueBook from './components/issueBook/issueBook';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <Home />
+     <IssueBook />
     </div>
   );
 }
