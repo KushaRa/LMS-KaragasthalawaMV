0069513b17067bc67c43300575705f3bf690a8ff
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-12-04 00:28:32 +0530

    changed some css

--- src/components/issueBook/issueBook.jsx
+++ src/components/issueBook/issueBook.jsx
@@ -1,52 +1,49 @@
+// 
+
 import React from 'react';
 import './issueBook.css';
 
-const IssueBook = () => {
+function IssueBook() {
   return (
-    <div className="issue-book-page">
-      <div className="issue-book-header">
-        <h3>Issue Book</h3>
-        <button className="close-button">×</button>
-        <form className="issue-book-form">
-        <div className="form-row">
-           <div className="form-group">
-            <label htmlFor="book-id">Book ID</label>
-            <input type="text" id="book-id" placeholder="Enter Book ID" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="member-id">Member ID</label>
-            <input type="text" id="member-id" placeholder="Enter Member ID" />
-          </div>
-        </div>
-        <div className="form-row">
-          <div className="form-group">
-            <label htmlFor="book-name">Book Name</label>
-            <input type="text" id="book-name" placeholder="Enter Book Name" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="member-name">Member Name</label>
-            <input type="text" id="member-name" placeholder="Enter Member Name" />
-          </div>
-        </div>
-        <div className="form-row">
-          <div className="form-group">
-            <label htmlFor="start-issue-date">Start Issue Date</label>
-            <input type="date" id="start-issue-date" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="end-issue-date">End Issue Date</label>
-            <input type="date" id="end-issue-date" />
-          </div>
-        </div>
-        <button type="submit" className="issue-book-button">Issue Book</button> 
-      </form>  
+    
+    <div className="issue-book-container">
+      <h2 className="issue-book-title">Issue Book</h2>
 
-
-
+      {/* First Container */}
+      <div className="form-section">
+        <div className="input-group">
+          <label htmlFor="bookId">Book ID</label>
+          <input type="text" id="bookId" placeholder="Enter Book ID" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="bookName">Book Name</label>
+          <input type="text" id="bookName" placeholder="Enter Book Name" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="memberId">Member ID</label>
+          <input type="text" id="memberId" placeholder="Enter Member ID" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="memberName">Member Name</label>
+          <input type="text" id="memberName" placeholder="Enter Member Name" />
+        </div>
       </div>
-      
+
+      {/* Second Container */}
+      <div className="form-section">
+        <div className="input-group">
+          <label htmlFor="startIssueDate">Start Issue Date</label>
+          <input type="date" id="startIssueDate" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="endIssueDate">End Issue Date</label>
+          <input type="date" id="endIssueDate" />
+        </div>
+      </div>
+
+      <button className="issue-book-button">Issue Book</button>
     </div>
   );
-};
+}
 
 export default IssueBook;
