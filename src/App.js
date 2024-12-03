ffa4eff4a98e35ebe7c68f29fef4611c2d9d81ef
ravindu <ravindulakshitha250@gmail.com>
2024-12-03 22:44:38 +0530

    update priter in files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import './App.css';
+import "./App.css";
 
-import NavBar from './components/navBar';
-import Login from './pages/Login/login';
+import NavBar from "./components/navBar";
+import Login from "./pages/Login/login";
 
-import Home from './pages/Home';
-import IssueBook from './components/issueBook/issueBook';
-
-
+import Home from "./pages/Home";
+import IssueBook from "./components/issueBook/issueBook";
 
 function App() {
   return (
     <div className="App">
-
-     <NavBar/>
-     <Login />
-
-    <Home/>
-     <IssueBook  />
-    
+      <NavBar />
+      <Login />
+      <Home />
+      <IssueBook />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import NavBar from '../components/Navbar/navBar'; 
+import React from "react";
+import NavBar from "../components/Navbar/navBar";
 import SearchIcon from "@mui/icons-material/Search";
-import './Home.css';
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-    <NavBar/>
-    <div className='home'>        
-      <div className='buttonSection'>
-        <span>Add/Issue/Return Books</span>
-        <button className='AddBook'>Add Book</button>
-        <button className='IssueBook'>Issue Book</button>
-        <button className='ReturnBook'>Return Book</button>
-      </div>
+      <NavBar />
+      <div className="home">
+        <div className="buttonSection">
+          <span>Add/Issue/Return Books</span>
+          <button className="AddBook">Add Book</button>
+          <button className="IssueBook">Issue Book</button>
+          <button className="ReturnBook">Return Book</button>
+        </div>
 
-      <div className='summaryTable'>
-        
-        <div className='searchbar'>
-        <input className='search1' placeholder='Sarch For The Book'></input>
-        <button><SearchIcon style={{ color: "black",marginTop:'10px'}} /></button>
+        <div className="summaryTable">
+          <div className="searchbar">
+            <input className="search1" placeholder="Sarch For The Book"></input>
+            <button>
+              <SearchIcon style={{ color: "black", marginTop: "10px" }} />
+            </button>
+          </div>
         </div>
       </div>
-      
-    </div>
     </>
   );
 };
