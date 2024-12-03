eae87203f693396ca0c4294b11bb934ba5b81070
ravindu <ravindulakshitha250@gmail.com>
2024-12-03 17:43:10 +0530

    create login folder and install packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './components/navBar';
+import Login from './pages/Login/login';
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Login />
     </div>
   );
 }
