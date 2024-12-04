aa4ba2804031fb37a367ca6cb50a713953b70a23
Lakshika Bnadara <lakshikabandara601@gmail.com>
2024-12-04 16:00:09 +0530

    changed the location of issueBook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavBar from './components/navBar';
-import IssueBook from './components/issueBook/issueBook';
+import IssueBook from './pages/issueBook/issueBook';
 
 function App() {
   return (
