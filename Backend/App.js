13a40f7d04bf8c3a0f2d20f64b72e233542f6fed
ravindu <ravindulakshitha250@gmail.com>
2024-12-08 02:56:11 +0530

    conect database with Backend part

--- Backend/App.js
+++ Backend/App.js
@@ -1,13 +1,10 @@
-require("dotenv").config();
+require("dotenv").config(); 
 require("./DB/connection");
 
 const express = require("express");
 const app = express();
-
-require("dotenv").config();
 const port = process.env.PORT;
 
-require("./DB/connection");
 app.use(express.json());
 
 app.get("/", (req, res) => {
