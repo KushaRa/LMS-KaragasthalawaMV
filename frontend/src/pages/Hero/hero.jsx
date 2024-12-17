963ea394ec4aedcd0ec103d3c918f2b8dce6989e
kaumadisandakal <121716599+kaumadisandakal@users.noreply.github.com>
2024-12-17 12:46:56 +0530

    Hero page started

--- frontend/src/pages/Hero/hero.jsx
+++ frontend/src/pages/Hero/hero.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./hero.css";
+import Navbar from "../../components/Navbar/navBar";
+
+
 
 const Hero = () => {
-  return <>
-  </>;
+  return (<>
+ <Navbar />
+ <h1>Welcome to Karagastalawa MV </h1>
+
+  </>
+  ); 
 };
 
 export default Hero;
