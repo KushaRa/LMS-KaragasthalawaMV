dce9e45083ec95770e01f95643bec515dfec3337
kushana <rashmisenadheera98@gmail.com>
2024-12-30 16:46:25 +0530

    Created GET request in frontend

--- frontend/src/pages/BookTable/bookTable.jsx
+++ frontend/src/pages/BookTable/bookTable.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React ,{useState, useEffect} from 'react';
 import './bookTable.css'
+import axios from 'axios';
+
 
 const BookTable = () => {
+  const [data,setData]=useState([]);
+  const [error, setError] = useState('');
+
+ useEffect (()=>{
+  const fetchData = async() =>{
+
+  try{
+    const response =await axios.get('http://localhost:5000/api/display-book');
+    setData(response.data);
+    
+  }catch(error){
+    setError(error.message);
+  }
+ };
+ fetchData();
+},[]);
+
+//if (loading) return <div>Loading...</div>;
+//if (error) return <div>Error: {error}</div>;
+
   return (
     <div className='bookDisplay'>
       <table>
@@ -10,10 +32,21 @@ const BookTable = () => {
             <th>Book Name</th>
             <th>Author</th>
             <th>Publisher</th>
-            <th>Entry Date</th>
             <th>Category</th>
             <th>Status</th>
         </tr>
+        <tbody>
+      {data.map((item, index) => (
+        <tr key={index}>
+          <td>{item.bookID}</td>
+          <td>{item.bookName}</td>
+          <td>{item.author}</td>
+          <td>{item.publisher}</td>
+          <td>{item.category}</td>
+          <td>{item.status}</td>
+        </tr>
+      ))}
+</tbody>
       </table>
     </div>
   );
